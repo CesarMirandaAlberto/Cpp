a7f5c8bd55c034a10b8c8477299a22ea98d3577b
CesarMirandaAlberto <cesarmialberto04@gmail.com>
2025-10-23 21:45:50 -0600

    Hola Mundo

---
+++ Fundamentos/Hola_Mundo.cpp
@@ -0,0 +1,18 @@
+/*
+        ***** HOLA MUNDO ****
+
+    El presente código implementa el hola mundo en C++
+
+*/
+
+//Inclusión de libreria
+#include <iostream> 
+
+// std:: Contiene nombres de los objetos estandar de C++ std(standard) los manda llamar
+int main()
+{
+    std::cout<<"!Hola Mundo!"; // Imprime el texto entre comillas mediante cout
+    std::cout<<"\ndesde C++";
+
+    return 0; //Fin del programa
+}
